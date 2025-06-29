4d59c5b644d0180ce4659b0dea6efd9205ed1428
nemecro <nemecstudies@gmail.com>
2025-06-29 19:55:40 +0200

    implement a check victory function

--- main.js
+++ main.js
@@ -113,7 +113,46 @@ const playerHandler = function(){
 }();
 
 // until a victory condition is not met, swap players turns and get their input
-const createGame = function(){
+const createGame = function(player1, player2){
+
+    function checkVictory(board) {
+        // Check rows and columns
+        for (let i = 0; i < 3; i++) {
+            if (
+            board[i][0] !== 0 &&
+            board[i][0] === board[i][1] &&
+            board[i][1] === board[i][2]
+            ) return board[i][0];
+            if (
+            board[0][i] !== 0 &&
+            board[0][i] === board[1][i] &&
+            board[1][i] === board[2][i]
+            ) return board[0][i];
+        }
+        // Check diagonals
+        if (
+        board[0][0] !== 0 &&
+        board[0][0] === board[1][1] &&
+        board[1][1] === board[2][2]
+        ) return board[0][0];
+
+        if (
+        board[0][2] !== 0 &&
+        board[0][2] === board[1][1] &&
+        board[1][1] === board[2][0]
+        ) return board[0][2];
+        return 0; // No winner
+    }
+
+    function play(){
+        gameboard.generateGameboard();
+        let activePlayer = player1;
+        do{
+            activePlayer = activePlayer === player1 ? player2 : player1;
+            gameboard.setGameboardField(activePlayer.symbol, prompt(`${activePlayer}'s turn row: `), prompt(`${activePlayer}'s turn column: `));
+        } while (checkVictory(gameboard.getGameboardStatus) === 0)
+    }
+
 
 }
 
